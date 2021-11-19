a8fadc20501882d02cf99c4891adde9ddda5c0f4
Fernando Martin Garcia Del Angel <mxg0820@dexcom.com>
2021-11-19 15:35:27 -0800

    Initial Commit
    - SwiftUI-based project

---
+++ ZebraHealth/ZebraHealth/ZebraHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZebraHealthApp.swift
+//  ZebraHealth
+//
+//  Created by mxg0820 on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ZebraHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
